0965a6a304febcae0da2c9f650be9248981f0875
Michael Colonna <michaelcolonna@Michaels-MacBook-Pro-4.local>
2017-12-02 18:17:47 -0500

    getArticleText() is now working

--- background.js
+++ background.js
@@ -4,10 +4,12 @@ document.addEventListener('DOMContentLoaded', () => {
     //chrome.identity.launchWebAuthFlow
 
     // TODO: get calculated information
+    // const a1 = new ArticleInfo("https://www.nytimes.com/2017/12/01/us/politics/senate-tax-bill.html?hp&action=click&pgtype=Homepage&clickSource=story-heading&module=span-ab-top-region&region=top-news&WT.nav=top-news");
+    // console.log(a1.getArticleText());
 
     // TODO: send info in POST request to server, receive info back, display
     const postParameters = {
-        
+
     }
 
 
--- text_extract.js
+++ text_extract.js
@@ -4,65 +4,29 @@ class ArticleInfo {
   }
 
 
-getArticleText()
-{
-  var token = process.env.articleAPIKEY;
-  var request = "https://api.diffbot.com/v3/article?token="+ token + "&url=" + this.url + "&fields=text";  
-  const https = require("https");
-  https.get(request, res => {
-    res.setEncoding("utf8");
-    let body = "";
-    res.on("data", data => {
-      body += data;
-    });
-    res.on("end", () => {
-      body = JSON.parse(body);
-      return body.results[0];
-    });
-  });
-    
-    
-}
+  getArticleText()
+  {
+      var token = "9475d0089ea474ac8ecd03d2b12d90f2";
+      console.log(process.env.articleAPIKEY);
+      var sUrl = "https://api.diffbot.com/v3/article?token="+ token + "&url=" + this.url;
+      $.get(sUrl, res => {
+          return res.objects[0].text;
+      })
+  }
 
-getArticleMins(text)
-{
-    var avg_wpm = 200;
-    return wordCount(text)/avg_wpm;
+  getArticleMins(text)
+  {
+      var avg_wpm = 200;
+      return wordCount(text)/avg_wpm;
+
+  }
+
+  wordCount(text) {
+      var count_text = text.split(" ");
+      return count_text.length();
+  }
 
 }
-
-wordCount(text) {
-  var count_text = text.split(" ");
-  return count_text.length();
-}
-
-}
-
-const a1 = new ArticleInfo("https://www.nytimes.com/2017/12/01/us/politics/senate-tax-bill.html?hp&action=click&pgtype=Homepage&clickSource=story-heading&module=span-ab-top-region&region=top-news&WT.nav=top-news");
+console.log("hi")
+const a1 = new ArticleInfo("http://www.nytimes.com/2017/12/01/us/politics/senate-tax-bill.html");
 console.log(a1.getArticleText());
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
